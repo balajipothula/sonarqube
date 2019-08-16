18f3b9f1ab22ba5ecedbf8c0bf66d9a38678ca44
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-16 16:50:14 +0530

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -7,18 +7,12 @@
 # updating rhel packages.
 sudo yum -y update
 
-# installing openjdk8.
-sudo yum -y install unzip java-1.8.0-openjdk-devel.x86_64
+curl -J -L http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm -o mysql.rpm
+sudo rpm -i mysql.rpm
 
-mkdir root                                                                                                         && \
-curl -J -L https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.2.1.zip -o $HOME/root/sonarqube.zip && \
-unzip   $HOME/root/sonarqube.zip -C $HOME/root/                                                                    && \
-rm -rf  $HOME/root/nexus.tar.gz                                                                                    && \
-mv      $HOME/root/nexus-*       $HOME/root/.nexusoss2                                                             && \
-mv      $HOME/root/sonatype-work $HOME/root/.artifactory                                                           && \
-find    $HOME/root/.nexusoss2     -maxdepth 1 -name "*.txt" -type f -delete                                        && \
-find    $HOME/root/.nexusoss2/bin -maxdepth 1 -name "*.bat" -type f -delete                                        && \
-find    $HOME/root/.artifactory   -maxdepth 1 -name "*.txt" -type f -delete                                        && \
-rm -rf  $HOME/root/.nexusoss2/bin/jsw/{license,linux-ppc*,linux-x86-32,macosx*,solaris*,windows*}                  && \
-sed -i 's/sonatype-work/.artifactory/g' $HOME/root/.nexusoss2/conf/nexus.properties                                && \
-sh      $HOME/root/.nexusoss2/bin/nexus start
+# installing openjdk8.
+sudo yum -y install unzip java-1.8.0-openjdk mysql-server
+
+
+curl -J -L https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.0.zip -o $HOME/sonarqube.zip && \
+unzip   $HOME/sonarqube.zip -C $HOME/
