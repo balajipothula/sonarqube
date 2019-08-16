f31a5ee17d4198ee8651d6943a1cf14a00c32768
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-16 12:39:01 +0530

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -10,15 +10,15 @@ sudo yum -y update
 # installing openjdk8.
 sudo yum -y install java-1.8.0-openjdk
 
-mkdir root                                                                                               && \
-curl -J -L https://download.sonatype.com/nexus/oss/nexus-latest-bundle.tar.gz -o $HOME/root/nexus.tar.gz && \
-tar xzf $HOME/root/nexus.tar.gz -C $HOME/root/                                                           && \
-rm -rf  $HOME/root/nexus.tar.gz                                                                          && \
-mv      $HOME/root/nexus-*       $HOME/root/.nexusoss2                                                   && \
-mv      $HOME/root/sonatype-work $HOME/root/.artifactory                                                 && \
-find    $HOME/root/.nexusoss2     -maxdepth 1 -name "*.txt" -type f -delete                              && \
-find    $HOME/root/.nexusoss2/bin -maxdepth 1 -name "*.bat" -type f -delete                              && \
-find    $HOME/root/.artifactory   -maxdepth 1 -name "*.txt" -type f -delete                              && \
-rm -rf  $HOME/root/.nexusoss2/bin/jsw/{license,linux-ppc*,linux-x86-32,macosx*,solaris*,windows*}        && \
-sed -i 's/sonatype-work/.artifactory/g' $HOME/root/.nexusoss2/conf/nexus.properties                      && \
+mkdir root                                                                                                         && \
+curl -J -L https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.2.1.zip -o $HOME/root/sonarqube.zip && \
+tar xzf $HOME/root/nexus.tar.gz -C $HOME/root/                                                                     && \
+rm -rf  $HOME/root/nexus.tar.gz                                                                                    && \
+mv      $HOME/root/nexus-*       $HOME/root/.nexusoss2                                                             && \
+mv      $HOME/root/sonatype-work $HOME/root/.artifactory                                                           && \
+find    $HOME/root/.nexusoss2     -maxdepth 1 -name "*.txt" -type f -delete                                        && \
+find    $HOME/root/.nexusoss2/bin -maxdepth 1 -name "*.bat" -type f -delete                                        && \
+find    $HOME/root/.artifactory   -maxdepth 1 -name "*.txt" -type f -delete                                        && \
+rm -rf  $HOME/root/.nexusoss2/bin/jsw/{license,linux-ppc*,linux-x86-32,macosx*,solaris*,windows*}                  && \
+sed -i 's/sonatype-work/.artifactory/g' $HOME/root/.nexusoss2/conf/nexus.properties                                && \
 sh      $HOME/root/.nexusoss2/bin/nexus start
