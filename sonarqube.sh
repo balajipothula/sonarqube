619933de628f3c5aeb585c8b847137afc56aa247
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-16 19:02:06 +0530

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -7,12 +7,20 @@
 # updating rhel packages.
 sudo yum -y update
 
-curl -J -L http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm -o mysql.rpm
+#  
+curl -J -L http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm -o mysql.rpm && \
 sudo rpm -i mysql.rpm
 
 # installing openjdk8.
-sudo yum -y install unzip java-1.8.0-openjdk mysql-server
+sudo yum -y install java-1.8.0-openjdk-devel.x86_64 mysql-server unzip
 
+# starting mysql daemon.
+sudo systemctl start mysqld
 
-curl -J -L https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.0.zip -o $HOME/sonarqube.zip && \
+#
+useradd sonar
+passwd  sonar
+
+https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-6.7.7.zip
+curl -J -L https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-6.7.7.zip -o $HOME/sonarqube.zip && \
 unzip   $HOME/sonarqube.zip -C $HOME/
