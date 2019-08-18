0c13827edfb116229c1979696cd8a3495c68dd7e
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-18 12:06:48 +0530

    Create adduser-sonar-db-user.sh

---
+++ adduser-sonar-db-user.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
+# Date        : 18 August 2019,
+# Description : Adding sonar user to postgresql db server.
+
+# creating new user.
+psql -c "CREATE USER sonar WITH PASSWORD 'sonar123';"
+
+# creating database owned by sonar.
+psql -c "CREATE DATABASE sonar OWNER sonar;"
