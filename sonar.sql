e8a7cdce6d784653c430b320be8d5eb23e2aed69
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-18 12:07:25 +0530

    Delete sonar.sql

--- sonar.sql
+++
@@ -1,8 +0,0 @@
--- changing sonar role password.
-ALTER USER sonar WITH ENCRYPTED password 's3cret';
-
--- creating sonar database.
-CREATE DATABASE sonar OWNER sonar;
-
--- reloading postgresql server configurations.
-SELECT pg_reload_conf();
