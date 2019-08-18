f698f93ecf475ac53d61807ac0fe767b737db5e5
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-18 13:19:59 +0530

    Update add-sonar-db-user.sh

--- add-sonar-db-user.sh
+++ add-sonar-db-user.sh
@@ -4,6 +4,9 @@
 # Date        : 18 August 2019,
 # Description : Adding sonar user to postgresql db server.
 
+# exits any line in the bash script fails.
+set -e
+
 # creating new user.
 psql -c "CREATE USER sonar WITH PASSWORD 'sonar123';"
 
