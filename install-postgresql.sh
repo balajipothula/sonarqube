d6e9b134229ab1141bf4df9106075475beea8601
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-18 11:59:01 +0530

    Update install-postgresql.sh

--- install-postgresql.sh
+++ install-postgresql.sh
@@ -7,29 +7,29 @@
 # Note        : Please run this file with root privilages.
 
 # updating rhel packages.
-yum -y update
+sudo yum -y update
 
 # installing postgresql repository.
-dnf -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+sudo dnf -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 
 # installing postgresql on rhel8.
-yum -y install @postgresql:9.6
+sudo yum -y install @postgresql:9.6
 
 # initializing postgresql db cluster.
-postgresql-setup --initdb
+sudo postgresql-setup --initdb
+
+# chaging pgsql directory owner to postgres.
+sudo chown -R postgres:postgres /var/lib/pgsql && chmod -R u=rwX,go= /var/lib/pgsql
 
 # changing methods peer -> trust and ident -> md5
 sed -i 's/peer/trust/g' /var/lib/pgsql/data/pg_hba.conf
 sed -i 's/ident/md5/g'  /var/lib/pgsql/data/pg_hba.conf
 
 # starting postgresql.
-systemctl start postgresql
+sudo systemctl start postgresql
 
 # starting postgresql at boot time.
-systemctl enable postgresql
-
-# chaging pgsql directory owner to postgres.
-chown -R postgres:postgres /var/lib/pgsql && chmod -R u=rwX,go= /var/lib/pgsql
+sudo systemctl enable postgresql
 
 # creating new user.
 psql -c "CREATE USER sonar WITH PASSWORD 'sonar123';"
