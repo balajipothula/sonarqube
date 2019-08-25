40255caf7a1e1dff5b6c8e11f19046aa847c0004
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-25 11:17:26 +0530

    Update install-sonarscanner.sh

--- install-sonarscanner.sh
+++ install-sonarscanner.sh
@@ -1,1 +1,20 @@
-https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.0.0.1744-linux.zip
+#!/bin/bash
+
+# Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
+# Date        : 16 August 2019,
+# Description : Installing sonar scanner on rhel.
+
+# https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.0.0.1744-linux.zip
+
+# exits any line in the bash script fails.
+set -e
+
+# updating rhel packages.
+sudo yum -y update
+
+# installing openjdk8 unzip.
+sudo yum -y install java-1.8.0-openjdk-devel.x86_64 unzip
+
+# downloading and running sonarscanner.
+curl -J -L https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.0.0.1744-linux.zip -o $HOME/sonar-scanner-cli.zip && \
+unzip -q $HOME/sonar-scanner-cli.zip -d $HOME/                                                                                                           && \
